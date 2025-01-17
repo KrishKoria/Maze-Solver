6f1d2c787b10cd186ecc1a053c98f0dae2571393
Krish Koria <krishkoria2004@gmail.com>
2025-01-17 11:33:23 +0000

    Add draw_move method to Cell class and update main drawing logic

--- main.py
+++ main.py
@@ -2,22 +2,29 @@ from window import Window
 from cell import Cell
 def main():
     window = Window(800, 600)
-    c = Cell(window)
-    c.left_wall = False
-    c.draw(50, 50, 100, 100)
+    c1 = Cell(window)
+    c1.right_wall = False
+    c1.draw(50, 50, 100, 100)
 
-    c = Cell(window)
-    c.right_wall = False
-    c.draw(125, 125, 200, 200)
+    c2 = Cell(window)
+    c2.left_wall = False
+    c2.bottom_wall = False
+    c2.draw(100, 50, 150, 100)
 
-    c = Cell(window)
-    c.bottom_wall = False
-    c.draw(225, 225, 250, 250)
+    c1.draw_move(c2)
 
-    c = Cell(window)
-    c.top_wall = False
-    c.draw(300, 300, 500, 500)
+    c3 = Cell(window)
+    c3.top_wall = False
+    c3.right_wall = False
+    c3.draw(100, 100, 150, 150)
 
+    c2.draw_move(c3)
+
+    c4 = Cell(window)
+    c4.left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
     window.wait_for_close()
 
 
