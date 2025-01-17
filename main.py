4752e4b52dfcc1b27c9e27f0923c4a02b3d1828a
Krish Koria <krishkoria2004@gmail.com>
2025-01-17 11:25:03 +0000

    Add Cell class for grid representation and update main drawing logic

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from window import Window
-from point import Point, Line
+from cell import Cell
 def main():
     window = Window(800, 600)
-    p1 = Point(100, 100)
-    p2 = Point(200, 200)
-    line = Line(p1, p2)
-    window.draw_line(line, 'red')
+    c = Cell(window)
+    c.left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(window)
+    c.right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(window)
+    c.bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(window)
+    c.top_wall = False
+    c.draw(300, 300, 500, 500)
 
     window.wait_for_close()
 
